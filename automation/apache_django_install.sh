ff014c693b998a5ae9bfaedcc7620c81e3161ccc
Ali Shandi <ashand01@seattlecentral.edu>
2017-02-25 13:06:50 -0800

    Update apache_django_install.sh

--- automation/apache_django_install.sh
+++ automation/apache_django_install.sh
@@ -27,7 +27,7 @@ cd /opt
 # we want to make this env accessible to the GCloud user because we don't want to have to run it as root
 
 sudo mkdir django
-sudo chown -R Jonathan django
+sudo chown -R ashand01 django
 sleep 5
 cd django
 sudo virtualenv django-env
@@ -41,7 +41,7 @@ echo "To switch out of virtualenv, type deactivate."
 echo "Now using this version of Python:"
 
 which python
-sudo chown -R Jonathan /opt/django
+sudo chown -R ashand01 /opt/django
 
 echo "Installing Django"
 
@@ -53,7 +53,7 @@ django-admin --version
 django-admin startproject project1
 
 echo "Adjusting settings.py allowed_hosts..."
-sed -i 's,ALLOWED_HOSTS = \[\],ALLOWED_HOSTS = \[*\],g' /opt/django/project1/project1/settings.py
+sed -i 's,ALLOWED_HOSTS = \[\],ALLOWED_HOSTS = \[\'*\'\],g' /opt/django/project1/project1/settings.py
 
 echo "This is the new django project directory..."
 
@@ -76,70 +76,3 @@ cd /opt/django/project1
 #python manage.py migrate
 
 echo "Django is now accessible from the web at [server IP]:8000..."
-
-#prepare django for postgresql integration -- install postgres dev packages
-
-sudo yum -y install python-devel postgresql-devel
-sudo yum -y install gcc
-
-#install psycopg2 to allow us to use the project1 database on postgres server
-
-pip install psycopg2
-
-#configure django database settings
-
-sed -i "s/        'ENGINE': 'django.db.backends.sqlite3',/        'ENGINE': 'django.db.backends.postgresql_psycopg2',/g" /opt/django/project1/project1/settings.py
-sed -i "s/        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/        'NAME': 'project1',/g" /opt/django/project1/project1/settings.py
-sed -i "80i 'USER': 'project1'," /opt/django/project1/project1/settings.py
-sed -i "81i 'PASSWORD': 'P@ssw0rd1'," /opt/django/project1/project1/settings.py
-sed -i "82i 'HOST': '10.128.0.6'," /opt/django/project1/project1/settings.py
-sed -i "83i 'PORT': '5432'," /opt/django/project1/project1/settings.py
-sed -i "s/'USER': 'project1',/        'USER': 'project1',/g" /opt/django/project1/project1/settings.py
-sed -i "s/'PASSWORD': 'P@ssw0rd1',/        'PASSWORD': 'P@ssw0rd1',/g" /opt/django/project1/project1/settings.py
-sed -i "s/'HOST': '10.128.0.6',/        'HOST': '10.128.0.6',/g" /opt/django/project1/project1/settings.py
-sed -i "s/'PORT': '5432',/        'PORT': '5432',/g" /opt/django/project1/project1/settings.py
-
-
-#migrate databasae
-
-cd /opt/django/project1
-python manage.py makemigrations #*******
-python manage.py migrate
-
-#create user
-
-python manage.py createsuperuser #<-- will allow admin login
-#manage.py docs for automataing
-python manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('ali', 'ashand01@seattlecentral.edu', 'P@ssw0rd1')" | python manage.py shell
-
-#start djanngo server in the background <-- use fg to bring the process to the foreground and ctrl-c to quit
-python manage.py runserver 0.0.0.0:8000&
-
-#python manage.py runserver 0.0.0.0:8000
-#http://server_domain_or_IP:8000/admin
-#
-#
-# (django-env) [Jonathan@django-a-centos7 project1]$ python manage.py migrate
-# Operations to perform:
-#   Apply all migrations: admin, auth, contenttypes, sessions
-# Running migrations:
-#   Applying contenttypes.0001_initial... OK
-#   Applying auth.0001_initial... OK
-#   Applying admin.0001_initial... OK
-#   Applying admin.0002_logentry_remove_auto_add... OK
-#   Applying contenttypes.0002_remove_content_type_name... OK
-#   Applying auth.0002_alter_permission_name_max_length... OK
-#   Applying auth.0003_alter_user_email_max_length... OK
-#   Applying auth.0004_alter_user_username_opts... OK
-#   Applying auth.0005_alter_user_last_login_null... OK
-#   Applying auth.0006_require_contenttypes_0002... OK
-#   Applying auth.0007_alter_validators_add_error_messages... OK
-#   Applying auth.0008_alter_user_username_max_length... OK
-#   Applying sessions.0001_initial... OK
-# (django-env) [Jonathan@django-a-centos7 project1]$ python manage.py createsuperuser
-# Username (leave blank to use 'jonathan'):
-# Email address: jwade005@seattlecentral.edu
-# Password:
-# Password (again):
-# Superuser created successfully.
